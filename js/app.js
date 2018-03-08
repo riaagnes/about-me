3300e8ebb38213363b059047ffec0671f9f7c6ba
ria agnes jose <joseagnesria@gmail.com>
2018-03-07 17:03:44 -0800

    changed styles, added ordered and unordered list,used For loop in 6th and 7th question

--- js/app.js
+++ js/app.js
@@ -53,6 +53,61 @@ else{
   alert('Very good!!! keep coding!!!');
   console.log(userName + ' likes coding');
 }
+alert('You have 4 guesses.');
+var myAge = 22;
+var guess;
+var correct =[];
+for(var i=0;i<4;i++){
+  console.log(i);
+  guess = prompt('Can you guess my age?');
+  guess = parseInt(guess);
+  if(guess === 22){
+    alert('Correct');
+    correct.push(1);
+    break;}
+  else if (guess<21){
+    alert('Your guess is too low,');
+  }
+  else{
+    alert('Your guess is too high');
+  }
+}
+var possibilities =['amazon','google','microsoft','apple'];
+var futureJob = 'google';
+var trial;
+alert('You have 6 guesses');
+for(var j = 0; i<6; i++){
+  console.log(j);
+  trial = prompt('Can you guess the company where i am gonna work in future').toLowerCase();
+  if(trial === possibilities[0]){
+    alert('yea!! I will be working with '+possibilities[0]);
+    correct.push(1);
+    console.log('I will be working with either one of these companies'+ possibilities[0],possibilities[1],possibilities[2], possibilities[3]);
+    break;
+  }
+  else if(trial === possibilities[1]){
+    alert('yea!! I will be working with ' + possibilities[1]);
+    correct.push(1);
+    console.log('I will be working with either one of these companies '+ possibilities[0],possibilities[1],possibilities[2],possibilities[3] );
+    break;
+  }
+  else if(trial === possibilities[2]){
+    alert('yea!! I will be working with ' + possibilities[2]);
+    correct.push(1);
+    console.log('I will be working with either one of these companies '+ possibilities[0],possibilities[1],possibilities[2],possibilities[3]);
+    break;
+  }
+  else if(trial === possibilities[3]){
+    alert('yea!! I will be working with '+ possibilities[3]);
+    correct.push(1);
+    console.log('I will be working with either one of these companies '+ possibilities[0],possibilities[1],possibilities[2],possibilities[3]);
+    break;
+  }
+  else{
+    alert('No,I will not be working with that company');
+  }
+}
+alert('Well done!! You got '+ correct.length +' out of 2 question answers correct ' + userName );
 
 
 
